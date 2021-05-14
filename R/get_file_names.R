76497374f857ca75e58bc8ba3f7d90f2bba52717
lorenzbr <lorenz.brachtendorf@gmx.de>
2021-05-14 21:52:56 +0200

    get file names function

--- R/get_file_names.R
+++ R/get_file_names.R
@@ -12,9 +12,12 @@ get_file_names <- function(path){
   file.previous = "previous_investments.csv"
   file.tickers <- "isin_ticker.csv"
   file.dividend.history <- "dividends_fullhistory.csv"
+  file.dividend.year <- "dividends_by_year.csv"
+  file.dividend.month <- "dividends_by_month.csv"
   file.target.shares <- "default_target_shares.csv"
   file.stats <- "portfolio_stats.csv"
-
+  file.returns.annual <- "annual_returns.csv"
+  file.returns.annualized <- "annualized_returns.csv"
 
   output.list <- list(
     file.transactions = file.transactions,
@@ -22,8 +25,12 @@ get_file_names <- function(path){
     file.previous = file.previous,
     file.tickers = file.tickers,
     file.dividend.history = file.dividend.history,
+    file.dividend.year = file.dividend.year,
+    file.dividend.month = file.dividend.month,
     file.target.shares = file.target.shares,
-    file.stats = file.stats
+    file.stats = file.stats,
+    file.returns.annual = file.returns.annual,
+    file.returns.annualized = file.returns.annualized
   )
 
   return(output.list)
